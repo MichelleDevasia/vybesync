73992aaf851350d81c3456622c632e652908dc40
Michelle <michelle.devasia@btech.chrsituniversity.in>
2026-08-11 14:39:07 +0530

    Add reload_gunicorn endpoint

--- routes/api.py
+++ routes/api.py
@@ -24,3 +24,8 @@ api_bp.route('/songs/<int:song_id>/purge', methods=['DELETE'])(token_required(So
 # Media Storage Streaming (Unprotected for simple HTML5 Audio tag streams)
 api_bp.route('/storage/<category>/<int:song_id>/<filename>', methods=['GET'])(SongController.stream_storage_file)
 api_bp.route('/test_ytdlp', methods=['GET'])(SongController.test_ytdlp)
+
+@api_bp.route('/reload_gunicorn', methods=['GET'])
+def reload_gunicorn():
+    import os
+    os._exit(0)
